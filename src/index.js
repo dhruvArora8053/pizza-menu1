eed329cf76458aa40274160769277215e9605628
Dhruv Arora <dhruvarora.code@gmail.com>
2023-09-28 13:25:54 +0530

    implementing styles

--- src/index.js
+++ src/index.js
@@ -57,7 +57,9 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
+  const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+
+  return <h1 style={style}>Fast React Pizza Co.</h1>;
 }
 
 function Menu() {
@@ -73,9 +75,11 @@ function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
   const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
 
-  if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-  else alert("Sorry we're closed ");
+  //   if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  //   else alert("Sorry we're closed ");
 
   return <footer>{new Date().toLocaleTimeString}We're currently open</footer>;
 }
